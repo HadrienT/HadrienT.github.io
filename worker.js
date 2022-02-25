3aa2655a50afe86cab8c277016432e21bb6dd2d1
Hadrien <htramoni@enssat.fr>
2022-02-25 09:50:40 +0100

    correctif variable

--- worker.js
+++ worker.js
@@ -2,5 +2,5 @@ onmessage = function (e) {
     const result = [];
     result.push(Math.floor(Math.random() * 100));
     result.push(Math.floor(Math.random() * 100));
-    postMessage(workerResult);
+    postMessage(result);
 }
